776254d4ff0ae6bcfd98d2f8d8c4eff464b59c97
Ken Nyame <hmahaddev93@gmail.com>
2021-08-05 04:52:28 -0400

    first commit
    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// Use our caching lowercase func.
+let s:Character = "a"
+let st:Character = "A"
+
+let sss = String(UnicodeScalar(UInt8(65)))
+
+print("%d", st.asciiValue)
+print("%@", sss)
---
+++ MyPlayground.playground/Sources/Character+Custom.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension Character {
+    var asciiValue: Int {
+        get {
+            let s = String(self).unicodeScalars
+            return Int(s[s.startIndex].value)
+        }
+    }
+}
